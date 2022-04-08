14dba3aca83d3ab474ea76908245531960e7fce9
eierschaedl <geraldsteinbrugger@hotmail.com>
2022-04-08 15:29:56 +0200

    Class Appointment

---
+++ Webprojekt/Backend/Appointment.php
@@ -0,0 +1,14 @@
+<?php
+class Appointment {
+    public $id;
+    public $name;
+    public $date_start;
+    public $date_end;
+
+    function __construct($id, $name, $date_start, $date_end) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->date_start = $date_start;
+        $this->date_end = $date_end;
+    }
+}
