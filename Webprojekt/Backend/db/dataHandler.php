94b798bcf28f32ce701705f1660f9da7d07a888c
jassi <if21b145@technikum-wien.at>
2022-04-10 18:55:41 +0200

    appointment class angepasst

--- Webprojekt/Backend/db/dataHandler.php
+++ Webprojekt/Backend/db/dataHandler.php
@@ -5,14 +5,12 @@ class dataHandler{
         $res = $this->getDemoList();
         return $res;
     }
-
+/*
     private static function getDemoList(){
         $demoList = [
-            new appointment(1, "Webprojekt", "10.04.2022", "28.04.2022"),
-            new appointment(2, "breakfast", "10.04.2022 10:00", "10.04.2022 16:00"),
-            new appointment(3, "sleep", "10.04.2022 23:59", "11.04. 2022 09:00"),
         ];
         return $demoList;
     }
+*/
 }
 ?>
--- Webprojekt/Backend/models/appointment.php
+++ Webprojekt/Backend/models/appointment.php
@@ -2,13 +2,15 @@
 class appointment {
     public $id;
     public $name;
-    public $date_start;
-    public $date_end;
+    public $description;
+    public $creator;
+    public $active;
 
-    function __construct($id, $name, $date_start, $date_end) {
+    function __construct($id, $name, $description, $creator, $active) {
         $this->id = $id;
         $this->name = $name;
-        $this->date_start = $date_start;
-        $this->date_end = $date_end;
+        $this->description = $description;
+        $this->creator = $creator;
+        $this->active = $active;
     }
 }
